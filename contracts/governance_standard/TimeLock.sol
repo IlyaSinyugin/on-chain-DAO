f89619edff867f834c60215c992392461b6f2b31
Ilya <ilya@sinyugin.ru>
2022-09-06 11:25:36 +0200

    smart contract done

--- contracts/governance_standard/TimeLock.sol
+++ contracts/governance_standard/TimeLock.sol
@@ -1,1 +1,16 @@
-//
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/governance/TimelockController.sol";
+
+contract TimeLock is TimelockController {
+    // minDelay: How long you have to wait before executing
+    // proposers is the list of addresses that can propose
+    // executors: Who can execute when a proposal passes
+
+    constructor(
+        uint256 minDelay,
+        address[] memory proposers,
+        address[] memory executors
+    ) TimelockController(minDelay, proposers, executors) {}
+}
